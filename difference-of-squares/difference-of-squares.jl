9d8260edd7226b35c63fb64d944749cd3b87a9de
Daniel-EST <dd_santos@id.uff.br>
2020-09-21 00:06:27 -0300

    Simplifies code to use only analytical solution

--- difference-of-squares/difference-of-squares.jl
+++ difference-of-squares/difference-of-squares.jl
@@ -1,14 +1,8 @@
 "Square the sum of the first `n` positive integers"
-function square_of_sum(n)
-    sum(0:n)^2
-end
+square_of_sum(n::Int) = ((n * (n + 1))/2) ^ 2 |> Int
 
 "Sum the squares of the first `n` positive integers"
-function sum_of_squares(n)
-    mapreduce(x -> x^2, +, 0:n)
-end
+sum_of_squares(n::Int) = (n * (n + 1) * (2 * n + 1)) / 6 |> Int
 
 "Subtract the sum of squares from square of the sum of the first `n` positive ints"
-function difference(n)
-    square_of_sum(n) - sum_of_squares(n)
-end
+difference(n::Int) = square_of_sum(n) - sum_of_squares(n)
